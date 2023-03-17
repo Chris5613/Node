b21c055995f5fe6f10876c6525cd8891bcdb195e
Christian Wu <christianwu56@gmail.com>
2023-03-17 09:27:02 -0700

    finished User Auth with JWT working

---
+++ routers/users.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const {
+    register,
+    userLogin,
+    userSignout,
+    token
+}
+= require('../controllers/userControllers')
+
+const User = require('../models/User')
+const router = express.Router()
+
+router.get('/token', token)
+router.post('/register', register)
+router.post('/login', userLogin)
+router.put('/signout', userSignout)
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose')
+const userRoutes = require('./routers/users'); 
+
 
 const app = express(); 
-
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, res.method)
@@ -16,6 +17,9 @@ mongoose.connection.on('connected', () => {
     app.listen(process.env.PORT, () => console.log('DB connected and server is running on port', process.env.PORT));
 })
 mongoose.connection.on('error', err => {
-    console.log('MongoDB connection error' + err);
+    console.log('MongoDB connection error ' + err);
     process.exit();
 });
+
+
+app.use('/', userRoutes)
