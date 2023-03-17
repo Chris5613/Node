7283683c6fa450cf16afb55d115cbd8dc2774aaa
Christian Wu <christianwu56@gmail.com>
2023-03-16 23:36:07 -0700

    Node server is live and moongoose is connected to MongoDB

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express(); 
+
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, res.method)
+    next()
+})
+
+mongoose.connect(process.env.MONGO)
+mongoose.connection.on('connected', () => {
+    app.listen(process.env.PORT, () => console.log('DB connected and server is running on port', process.env.PORT));
+})
+mongoose.connection.on('error', err => {
+    console.log('MongoDB connection error' + err);
+    process.exit();
+});
